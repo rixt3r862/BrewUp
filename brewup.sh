cedd64fc7512f8dee490b56974cdb3b09e3c9486
Rick Mosher <rick.mosher@msn.com>
2022-05-27 19:04:12 -0600

    20220527.1903_update

--- brewup.sh
+++ brewup.sh
@@ -54,4 +54,4 @@ git add . 2>&1
 git commit -m "${DATE}_update" 2>&1
 git push 2>&1
 
-echo "${green}==> All Updates & Cleanups Finished${reset}"
+echo "${green}==> ${blue}All Updates & Cleanups Finished${reset}"
